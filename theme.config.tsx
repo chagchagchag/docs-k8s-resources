608214a391ffd89dab3ce900837cc692997ecbc6
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-31 00:52:12 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-k8s-resources',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-k8s-resources',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
